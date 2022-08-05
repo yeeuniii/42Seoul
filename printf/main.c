59d20a5f67a5cb2da7dc18ac31c02c521e11c1d8
Yeeun Park <yeepark@c10r1s8.42seoul.kr>
2022-08-05 15:45:22 +0900

    [ft_printf] test_code 추가

--- printf/main.c
+++ printf/main.c
@@ -1,36 +1,57 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yeepark <yeepark@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/27 17:13:34 by yeepark           #+#    #+#             */
-/*   Updated: 2022/07/27 19:33:38 by yeepark          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include <stdio.h>
-#include <limits.h>
 #include "ft_printf.h"
-
+#include <stdio.h>
 int	main(void)
 {
-//	int d = 1234;
-//	char c = 'a';
-//	char *s = "hello";
-//
-//	printf("\n%d\n", ft_printf("%d%i%c%s%p%x%X%%", d, d, c, s, &d, d, d));
-//	printf("\n%d\n", printf("%d%i%c%s%p%x%X%%", d, d, c, s, &d, d, d));
+	char	c = 'a';
+	
+	printf("\n%%c\n");
+	printf("%d\n", ft_printf("%c - ", c));
+	printf("%d\n", printf("%c - ", c));
+	
+	char	*s = 0;
+	
+	printf("\n%%s\n");
+	printf("%d\n", ft_printf("%s - ", s));
+	printf("%d\n", printf("%s - ", s));
+	
+	int num = 50;
+	int	min = -2147483648;
 
-//	printf("\n%d\n", ft_printf("%p", 0));
-//	printf("\n%d\n", printf("%p", (void *)0));
+	printf("\n%%d\n");
+	printf("%d\n", ft_printf("%d - ", num));
+	printf("%d\n", printf("%d - ", num));
+	
+	printf("\n%%i\n");
+	printf("%d\n", ft_printf("%i - ", min));
+	printf("%d\n", printf("%i - ", min));
+	
+	int	u = -50;
+	
+	printf("\n%%u\n");
+	printf("%d\n", ft_printf("%u\n", u));
+	printf("%d\n", printf("%u\n", u));
 
-//	int n=ft_printf(" %x %x %x %x %x %x %x\n", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX,0, -42);
-//	printf("%d\n", n);
-//	int m=printf(" %x %x %x %x %x %x %x\n", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX,0, -42);
-//	printf("%d\n", m);
+	printf("\n%%p\n");
+	printf("%d\n", ft_printf("%p\n", &num));
+	printf("%d\n", printf("%p\n", &num));
 
-	printf("\n%d\n", ft_printf("%s", NULL));
-	printf("\n%d\n", printf("%s", NULL));
-}
+
+
+	unsigned int x = 123456;
+	
+	printf("\n%%x\n");
+	printf("%d\n", ft_printf("%x\n", x));
+	printf("%d\n", printf("%x\n", x));
+	printf("\n%%X\n");
+	printf("%d\n", ft_printf("%X\n", x));
+	printf("%d\n", printf("%X\n", x));
+
+	int a1 = printf("%c aa %d aa %s aa %x aa %p aa \n", 'a', 'a', "abc", 123456, "a");
+	int a2 = ft_printf("%c aa %d aa %s aa %x aa %p aa \n", 'a', 'a', "abc", 123456, "a");
+	ft_printf ("\n%d %d\n\n", a1, a2 );
+
+	ft_printf("%x %x %x \n", 512, 512, 512);
+	ft_printf("%d %d %d \n", 512, 512, 512);
+	ft_printf("%p %p %p \n", &a1, &a1, &a1);
+}	
+
