175c166cdb04f8f8bb6f3a2ef6de546121175803
Yeeun Park <yeepark@c10r1s8.42seoul.kr>
2022-11-07 20:07:23 +0900

    feat(push_swap): b 스택에서 a 스택으로 이동하면서 정렬 && 정렬함수 구현 완료

--- Circle2/push_swap/srcs/sort.c
+++ Circle2/push_swap/srcs/sort.c
@@ -6,7 +6,7 @@
 /*   By: yeepark <yeepark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 16:47:21 by yeepark           #+#    #+#             */
-/*   Updated: 2022/11/07 19:09:56 by yeepark          ###   ########.fr       */
+/*   Updated: 2022/11/07 19:48:19 by yeepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,69 @@ void	make_sandglass(t_stack **a, t_stack **b, int chunk)
 			push_stack(b, a, 'b');
 			idx ++;
 		}
-		else if (idx <= ranking && ranking < idx + chunk)
+		if (idx <= ranking && ranking < idx + chunk)
 		{
 			push_stack(b, a, 'b');
 			rotate_stack(b, 'b');
 			idx ++;
 		}
-		else if (ranking >= idx + chunk)
+		if (ranking >= idx + chunk)
 			rotate_stack(a, 'a');
+	}
+}
+
+t_node	*find_highest_ranking_node(t_stack *stack, int ranking, int *reverse)
+{
+	int		idx;
+	int		size;
+	t_node	*top_node;
+	t_node	*bottom_node;
+
+	idx = 0;
+	size = stack->size;
+	top_node = stack->head;
+	bottom_node = stack->tail;
+	while (idx <= size / 2)
+	{
+		if (top_node->ranking == ranking)
+		{
+			*reverse = 0;
+			return (top_node);
+		}
+		if (bottom_node->ranking == ranking)
+		{
+			*reverse = 1;
+			return (bottom_node);
+		}
+		top_node = top_node->next;
+		bottom_node = bottom_node->prev;
+		idx ++;
+	}
+	return (0);
+}
+
+void	move_to_head(t_stack **stack, t_node *node, int reverse)
+{
+	void	(*func[2])(t_stack **, char);
+	
+	func[0] = rotate_stack;
+	func[1] = rotate_reverse_stack;
+	while ((*stack)->head != node)
+		func[reverse](stack, 'b');
+}
+
+void	b_to_a(t_stack **a, t_stack **b)
+{
+	int	size;
+	int	ranking;
+	int	reverse;
+
+	size = (*b)->size;
+	ranking = size;
+	while (ranking--)
+	{
+		move_to_head(b, find_highest_ranking_node(*b, ranking, &reverse), reverse);
+		push_stack(a, b, 'a');
 	}
 }
 
@@ -48,6 +103,7 @@ void	sort(t_stack **a, t_stack **b)
 
 	a_size = (*a)->size;
 	chunk = 0.000000053 * a_size * a_size + 0.3 * a_size + 14.5;
-	chunk = 2;
+//	chunk = 2;
 	make_sandglass(a, b, chunk);
+	b_to_a(a, b);
 }
