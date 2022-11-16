d79868a3af976e8a73062c19c789ce5df6fd551b
yeeuniii <anna862700@gmail.com>
2022-11-16 15:34:44 +0900

    fix(push_swap): 숫자 10자리 이상일때 처리(ex. 000000000000002)

--- Circle2/push_swap/bonus/check.c
+++ Circle2/push_swap/bonus/check.c
@@ -6,7 +6,7 @@
 /*   By: yeepark <yeepark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 14:56:20 by yeepark           #+#    #+#             */
-/*   Updated: 2022/11/13 03:18:48 by yeeun            ###   ########.fr       */
+/*   Updated: 2022/11/16 15:33:37 by yeepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,21 @@ int	ft_isinteger(char *str)
 	long	res;
 	int		idx;
 	int		sign;
-	int		digit;
 
 	res = 0;
 	idx = 0;
 	sign = 1;
-	digit = 0;
 	if (str[idx] == '+' || str[idx] == '-')
 	{
 		sign = (str[idx] == '+') - (str[idx] == '-');
 		idx ++;
 	}
-	while (ft_isdigit(str[idx]) && digit < 11)
+	while (ft_isdigit(str[idx]))
 	{
 		res = res * 10 + str[idx] - '0';
 		idx ++;
-		digit ++;
 	}
-	if ((!ft_isdigit(str[idx]) && str[idx]) || digit == 11)
+	if ((!ft_isdigit(str[idx]) && str[idx]))
 		return (0);
 	if ((sign == 1 && res > 2147483647) || (sign == -1 && res > 2147483648))
 		return (0);
