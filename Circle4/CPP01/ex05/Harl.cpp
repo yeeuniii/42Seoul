5dd01205e638152fa627379362f059d6d617239b
yeeuniii <anna862700@gmail.com>
2023-07-24 15:08:47 +0900

    Feat(CPP01): ex05 가독성있게 문구 수정 및 지정된 수준이 아닌 경우 메시지 추가

--- Circle4/CPP01/ex05/Harl.cpp
+++ Circle4/CPP01/ex05/Harl.cpp
@@ -3,22 +3,36 @@
 
 void	Harl::debug(void)
 {
-		std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger.\n"
+		<< "I really do!" << std::endl;
 }
 
 void	Harl::info(void)
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money.\n"
+		<< "You didn’t put enough bacon in my burger!\n"
+		<< "If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void	Harl::warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free.\n"
+		<< "I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void	Harl::error(void)
 {
-	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable!\n"
+		<< "I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::displayInvalid(void)
+{
+	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 }
 
 int		Harl::getIndex(std::string level)
@@ -39,6 +53,10 @@ void	Harl::complain(std::string level)
 	void	(Harl::*harl_functions[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
 	int		index = getIndex(level);
 
-	if (index != -1)
-		(this->*harl_functions[index])();
+	if (index == -1)
+	{
+		displayInvalid();
+		return ;
+	}
+	(this->*harl_functions[index])();
 }
--- Circle4/CPP01/ex05/Harl.hpp
+++ Circle4/CPP01/ex05/Harl.hpp
@@ -11,7 +11,7 @@ class Harl
 		void	info(void);
 		void	warning(void);
 		void	error(void);
-	
+		void	displayInvalid(void);
 		int		getIndex(std::string level);
 
 	public:
