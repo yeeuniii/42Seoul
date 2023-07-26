aaf9b484f2063496f7f3ee6903212a737dbd3dc9
yeeuniii <anna862700@gmail.com>
2023-07-26 20:35:03 +0900

    Test(CPP02): ex03 main문에 테스트케이스 추가

--- Circle4/CPP02/ex03/bsp.cpp
+++ Circle4/CPP02/ex03/bsp.cpp
@@ -16,9 +16,6 @@ bool	isTriangle(Point const a, Point const b, Point const c)
 	float	length_c = getLength(a, b);
 	bool	return_value;
 
-	// std::cout << length_a << std::endl;	
-	// std::cout << length_b << std::endl;	
-	// std::cout << length_c << std::endl;	
 	if (length_a == 0 || length_b == 0 || length_c == 0)
 		return false;
 	return_value = length_a < length_b + length_c ? true : false;
@@ -30,12 +27,9 @@ bool	isTriangle(Point const a, Point const b, Point const c)
 bool	isIn(Vector const d1, Vector const d2, Vector const p)
 {
 	Fixed	denominator = d1.getX() * d2.getY() - d2.getX() * d1.getY();
-	// std::cout << denominator << std::endl;
 	Fixed	t1 = (p.getX() * d2.getY() - p.getY() * d2.getX()) / denominator;
 	Fixed	t2 = (p.getX() * d1.getY() - p.getY() * d1.getX()) / denominator * -1;
 
-	// std::cout << "t1 = " << t1 << std::endl;	
-	// std::cout << "t2 = " << t2 << std::endl;	
 	return (t1 > 0 && t1 < 1) && (t2 > 0 && t2 < 1) && (t1 + t2 < 1);
 }   
 
--- Circle4/CPP02/ex03/main.cpp
+++ Circle4/CPP02/ex03/main.cpp
@@ -6,10 +6,17 @@ bool	bsp(Point const a, Point const b, Point const c, Point const point);
 
 int main(void)
 {
-	std::cout << bsp(Point(Fixed(), Fixed()), Point(Fixed(4), Fixed(0)), Point(Fixed(2), Fixed(4)), Point(Fixed(2), Fixed(2))) << std::endl;
-	std::cout << bsp(Point(Fixed(), Fixed()), Point(Fixed(4), Fixed(0)), Point(Fixed(2), Fixed(4)), Point(Fixed(20), Fixed(2))) << std::endl;
-	std::cout << bsp(Point(Fixed(), Fixed()), Point(Fixed(4), Fixed(0)), Point(Fixed(2), Fixed(4)), Point(Fixed(2), Fixed(4))) << std::endl;
-	std::cout << bsp(Point(Fixed(), Fixed()), Point(Fixed(4), Fixed(0)), Point(Fixed(2), Fixed(4)), Point(Fixed(3), Fixed(0))) << std::endl;
+	Point	a(Fixed(0), Fixed(0));
+	Point	b(Fixed(4), Fixed(0));
+	Point	c(Fixed(2), Fixed(4));
+	
+	std::cout << bsp(a, b, c, Point(Fixed(2), Fixed(2))) << std::endl;
+	
+	std::cout << bsp(a, b, c, Point(Fixed(20), Fixed(2))) << std::endl;
+	std::cout << bsp(a, b, c, Point(Fixed(2), Fixed(4))) << std::endl;
+	std::cout << bsp(a, b, c, Point(Fixed(3), Fixed(0))) << std::endl;
+	
+	std::cout << bsp(a, b, Point(Fixed(3), Fixed(0)), Point(Fixed(3), Fixed(0))) << std::endl;
 	
 	return 0;
 }
