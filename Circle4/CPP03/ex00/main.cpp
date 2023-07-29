6c44671050e24a679e09a2f195fc589e343477b0
yeeuniii <anna862700@gmail.com>
2023-07-29 11:31:18 +0900

    Test(CPP03): ex00 test main문 수정

--- Circle4/CPP03/ex00/main.cpp
+++ Circle4/CPP03/ex00/main.cpp
@@ -1,34 +1,39 @@
 #include "ClapTrap.hpp"
 #include <iostream>
 
-int	main(void)
+int main(void)
 {
-	ClapTrap	claptrap1("1");
-	ClapTrap	claptrap2("2");
-
-	std::cout << std::endl;
-	claptrap1.displayStatus();
-	claptrap2.displayStatus();
-
-	claptrap2.attack(claptrap1.getName());
-	claptrap1.takeDamage(claptrap2.getAttackDamage());
-	claptrap1.takeDamage(50);
-	claptrap1.beRepaired(40);
-	std::cout << std::endl;
-
-	claptrap1.displayStatus();
-	claptrap2.displayStatus();
-	std::cout << std::endl;
-
-	claptrap2.setAttackDamage(5);
-	for (int idx = 0; idx < 10; idx++)
+	std::cout << "----------TEST1----------" << std::endl;
 	{
+		ClapTrap claptrap1("one");
+		ClapTrap claptrap2("two");
+
+		claptrap1.displayStatus();
+		claptrap2.displayStatus();
+
 		claptrap2.attack(claptrap1.getName());
 		claptrap1.takeDamage(claptrap2.getAttackDamage());
+		claptrap1.beRepaired(40);
+
+		claptrap1.displayStatus();
+		claptrap2.displayStatus();
 	}
-	claptrap1.beRepaired(10);
-	claptrap1.displayStatus();
-	claptrap2.displayStatus();
-	
+	std::cout << std::endl;
+	std::cout << "----------TEST2----------" << std::endl;
+	{
+		ClapTrap claptrap1("one");
+		ClapTrap claptrap2("two");
+
+		claptrap2.setAttackDamage(1);
+		for (int idx = 0; idx < 10; idx++)
+		{
+			claptrap2.attack(claptrap1.getName());
+			claptrap1.takeDamage(claptrap2.getAttackDamage());
+		}
+		claptrap1.beRepaired(10);
+		claptrap2.attack(claptrap1.getName());
+		claptrap1.displayStatus();
+		claptrap2.displayStatus();
+	}
 	return 0;
 }
