dc00a29eacff22da7213d1652b597197edf82e80
yeeuniii <anna862700@gmail.com>
2023-08-05 15:30:44 +0900

    Test(CPP04): ex00 test case 추가

--- Circle4/CPP04/ex00/main.cpp
+++ Circle4/CPP04/ex00/main.cpp
@@ -5,19 +5,36 @@
 
 int main()
 {
+	std::cout << "---------------TEST1---------------" << std::endl;
 	{
-		const Animal *meta = new Animal();
+		const Animal *base = new Animal();
 		const Animal *dog = new Dog();
 		const Animal *cat = new Cat();
-		// std::cout << j->getType() << " " << std::endl;
-		// std::cout << i->getType() << " " << std::endl;
-		// i->makeSound(); // will output the cat sound!
-		// j->makeSound();
-		// meta->makeSound();
-		delete	meta;
+		std::cout << "Expected type is Dog : " << dog->getType() << std::endl;
+		std::cout << "Expected type is Cat : " << cat->getType() << std::endl;
+		dog->makeSound();
+		cat->makeSound(); 
+		base->makeSound();
+		delete	base;
 		delete	dog;
 		delete	cat;
 	}
 	std::cout << std::endl;
+	std::cout << "---------------TEST2---------------" << std::endl;
+	{
+		Animal	*animals[3];
+
+		animals[0] = new Animal();
+		animals[1] = new Cat();
+		animals[2] = new Dog();
+		for (int idx = 0; idx < 3; idx++)
+		{
+			std::cout << "Type is " << animals[idx]->getType()
+					<< ", and make sound like this : ";
+			animals[idx]->makeSound();
+		}
+		for (int idx = 0; idx < 3; idx++)
+			delete animals[idx];
+	}
 	return 0;
 }
