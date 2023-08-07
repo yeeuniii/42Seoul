67bf33b4a0d3faa45535d61e0dc7a8f34b16bab9
yeeuniii <anna862700@gmail.com>
2023-08-08 02:54:20 +0900

    Feat(CPP04): ex03 Character 클래스 생성 및 메서드 구현

---
+++ Circle4/CPP04/ex03/Character.cpp
@@ -0,0 +1,77 @@
+#include "Character.hpp"
+
+Character::Character()
+{
+	this->name = "anonymous";
+	this->inventory = new AMateria*[acceptableSize];
+	memset(this->inventory, 0, sizeof(AMateria*) * acceptableSize);
+	this->size = 0;
+}
+
+Character::Character(const std::string &name)
+{
+	this->name = name;
+	this->inventory = new AMateria*[acceptableSize];
+	memset(this->inventory, 0, sizeof(AMateria*) * acceptableSize);
+	this->size = 0;
+}
+
+Character::Character(const Character& character)
+{
+	this->inventory = new AMateria*[acceptableSize];
+	memset(this->inventory, 0, sizeof(AMateria*) * acceptableSize);
+	*this = character;
+}
+
+Character::~Character()
+{
+	for (int idx = 0; idx < size; idx++)
+		delete this->inventory[idx];
+	delete this->inventory;
+
+}
+
+Character&	Character::operator=(const Character& character)
+{
+	if (this == &character)
+		return *this;
+	this->name = character.name;
+	for (int idx = 0; idx < this->size; idx++)
+	{
+		this->inventory[idx] = character.inventory[idx]->clone();
+	}
+	this->size = character.size;
+	return *this;
+}
+
+std::string const& Character::getName() const
+{
+	return this->name;
+}
+
+void	Character::equip(AMateria* m)
+{
+	if (this->size == acceptableSize)
+		return ;
+	this->inventory[this->size] = m;
+	this->size++;
+}
+
+void	Character::unequip(int idx)
+{
+	if (idx < 0 || idx >= this->size)
+		return ;
+	this->inventory[idx] = 0;
+	while (idx < acceptableSize - 1)
+	{
+		this->inventory[idx] = this->inventory[idx + 1];
+		idx++;
+	}
+}
+
+void	Character::use(int idx, ICharacter& target)
+{
+	if (idx < 0 || idx >= this->size)
+		return ;
+	this->inventory[idx]->use(target);
+}
