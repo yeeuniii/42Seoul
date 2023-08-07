fe07b8da211c4cc6d401bb764d6537908b1d2b58
yeeuniii <anna862700@gmail.com>
2023-08-08 00:08:35 +0900

    Feat(CPP04): ex03 MateriaSource 구현 by OCCF

---
+++ Circle4/CPP04/ex03/MateriaSource.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef __MATERIASOURCE_HPP__
+#define __MATERIASOURCE_HPP__
+
+#include "IMateriaSouce.hpp"
+
+class MateriaSource : public IMateriaSource
+{
+	const static int	acceptableSize = 4;
+	private:
+		AMateria**	inventory;
+		int			size;
+
+	public:
+		MateriaSource();
+		MateriaSource(const MateriaSource &matriaSource);
+		~MateriaSource();
+		MateriaSource&	operator=(const MateriaSource &matriaSource);
+
+		virtual void	learmMateria(AMateria*);
+		virtual AMateria*	createMateria(std::string const& type);
+
+};
+
+#endif
