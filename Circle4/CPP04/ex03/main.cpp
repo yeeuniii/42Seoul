98f35fed7d92ea80585195966203b775d9674a70
yeeuniii <anna862700@gmail.com>
2023-08-08 13:50:20 +0900

    Test(CPP04): ex03 Character unequip 메서드 및 복사 생성자 깊은 복사 확인하는 테스트 추가

--- Circle4/CPP04/ex03/main.cpp
+++ Circle4/CPP04/ex03/main.cpp
@@ -5,28 +5,90 @@
 #include "MateriaSource.hpp"
 #include <iostream>
 
-void	leaks()
+void leaks()
 {
 	system("leaks a.out");
 }
 
-int main()
+void	subjectTest()
 {
-	// atexit(leaks);
 	IMateriaSource *src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
+
 	ICharacter *me = new Character("me");
+
 	AMateria *tmp;
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
+	
 	ICharacter *bob = new Character("bob");
+	
 	me->use(0, *bob);
 	me->use(1, *bob);
+	
 	delete bob;
 	delete me;
 	delete src;
+}
+
+void	myTest()
+{
+	IMateriaSource *src = new MateriaSource();
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+	src->learnMateria(new Ice());
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+	src->learnMateria(new Cure());
+
+	ICharacter *me = new Character("me");
+
+	AMateria *tmp;
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+
+	Character *mi = (Character *)me;
+	tmp = mi->getInventorySlot(0);	
+	me->unequip(0);
+	me->use(0, *me);
+	me->equip(tmp);
+	me->use(1, *me);
+
+	// delete tmp;
+	delete src;
+	delete me;
+}
+
+void	copyTest()
+{
+	IMateriaSource *src = new MateriaSource();
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+
+	ICharacter *me = new Character("me");
+
+	me->equip(new Ice());
+
+	Character *mi = (Character*)me;
+	Character you(*mi);
+	
+	you.use(0, you);
+
+	// delete you;
+	delete me;
+	delete src;
+}
+
+int main()
+{
+	atexit(leaks);
+	// subjectTest();
+	// myTest();
+	copyTest();
 	return 0;
 }
