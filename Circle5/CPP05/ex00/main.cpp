73666ada2e9a0a104dcdccc38bfe6b55245e36bc
yeeuniii <anna862700@gmail.com>
2023-08-20 20:51:10 +0900

    Test(CPP05): ex00 테스트문 수정

--- Circle5/CPP05/ex00/main.cpp
+++ Circle5/CPP05/ex00/main.cpp
@@ -1,18 +1,119 @@
 #include <iostream>
 #include "Bureaucrat.hpp"
 
-int	main()
+void	testConstructor()
 {
-	try 
+	std::cout << "----------Basic Test----------" << std::endl;
 	{
-		Bureaucrat bureaucrat1("name", 150);
-		Bureaucrat bureaucrat2(bureaucrat1);
-		Bureaucrat bureaucrat3("exception", -151);
+		try
+		{
+			Bureaucrat basic("basic", 150);
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+	std::cout << std::endl;
+	std::cout << "----------High Test----------" << std::endl;
+	{
+		try
+		{
+			Bureaucrat high("high", 151);
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+	std::cout << std::endl;
+	std::cout << "----------Low Test----------" << std::endl;
+	{
+		try
+		{
+			Bureaucrat low("low", 0);
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+	std::cout << std::endl;
+}
 
-	}
-	catch (std::exception &e)
+void	testIncrementGrade()
+{
+	std::cout << "----------Normal----------" << std::endl;
 	{
-		std::cout << e.what() << std::endl;
+		try
+		{
+			Bureaucrat one("one", 3);
+
+			one.incrementGrade(1);
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
 	}
+	std::cout << std::endl;
+	std::cout << "----------Handle exception----------" << std::endl;
+	{
+		try
+		{
+			Bureaucrat one("one", 3);
+
+			one.incrementGrade(3);
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}	
+	std::cout << std::endl;
+}
+
+void	testDecrementGrade()
+{
+	std::cout << "----------Normal----------" << std::endl;
+	{
+		try
+		{
+			Bureaucrat one("one", 140);
+
+			one.decrementGrade(5);
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+	std::cout << std::endl;
+	std::cout << "----------Handle exception----------" << std::endl;
+	{
+		try
+		{
+			Bureaucrat one("one", 149);
+
+			one.decrementGrade(5);
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}	
+	std::cout << std::endl;
+}
+int main()
+{
+	std::cout << "1. Construct" << std::endl;
+	testConstructor();
+	std::cout << std::endl;
+	std::cout << "2. Increment grade" << std::endl;
+	testIncrementGrade();
+	std::cout << std::endl;
+	std::cout << "3. Decrement grade" << std::endl;
+	testDecrementGrade();
+	std::cout << std::endl;
 	return 0;
 }
