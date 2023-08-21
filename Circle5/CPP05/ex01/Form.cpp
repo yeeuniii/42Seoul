eeb9db97e83784cf51409b098a19eda64eafa487
yeeuniii <anna862700@gmail.com>
2023-08-21 15:57:36 +0900

    Feat(CPP05): ex01 Form 클래스 메서드(getter 함수 / beSigned) 구현 및 출력 연산자 오버로딩

--- Circle5/CPP05/ex01/Form.cpp
+++ Circle5/CPP05/ex01/Form.cpp
@@ -1,4 +1,15 @@
-#include  "Form.hpp"
+#include "Form.hpp"
+#include <iostream>
+
+const char* Form::GradeTooHighException::what() const throw()
+{
+	return "Form`s grade is too high.";
+}
+
+const char* Form::GradeTooLowException::what() const throw()
+{
+	return "Form`s grade is too low.";
+}
 
 Form::Form(std::string name, int signableGrade, int executableGrade) :
 	name(name), signableGrade(signableGrade), executableGrade(executableGrade)
@@ -51,3 +62,41 @@ bool	Form::isLowGrade() const
 	return this->signableGrade < 1 || this->executableGrade < 1;
 }
 
+const std::string&	Form::getName() const
+{
+	return this->name;
+}
+
+const bool&	Form::getIsSigned() const
+{
+	return this->isSigned;
+}
+
+const int&	Form::getSignableGrade() const
+{
+	return this->signableGrade;
+}
+
+const int&	Form::getExecutableGrade() const
+{
+	return this->executableGrade;
+}
+
+void	Form::beSigned(Bureaucrat bureaucrat)
+{
+	this->isSigned = bureaucrat.getGrade() <= this->signableGrade;
+	if (this->isSigned == false)
+		throw Form::GradeTooLowException();
+}
+
+std::ostream&	operator<<(std::ostream &out, const Form &form)
+{
+	out << form.getName()
+		<< ", form signable grade " << form.getSignableGrade()
+		<< ", executable grade " << form.getExecutableGrade() << "." << std::endl;
+	out << "And it got signed";
+	if (form.getIsSigned() == false)
+		out << " fail";
+	out << "." << std::endl;;
+	return out;
+}
--- Circle5/CPP05/ex01/Form.hpp
+++ Circle5/CPP05/ex01/Form.hpp
@@ -1,28 +1,20 @@
 #ifndef __FORM_HPP__
-#define __FORM_HOO__
+#define __FORM_HPP__
 
 #include <string>
 #include <exception>
+#include "Bureaucrat.hpp"
 
-class Form
+class	Bureaucrat;
+
+class	Form
 {
 	private:
 		const std::string name;
 		bool			isSigned;
 		const int		signableGrade;
 		const int		executableGrade;
-
-		class GradeTooHighException : public std::exception
-		{
-			public:
-				virtual const char	*what() const throw();
-		};
-		class GradeTooLowException : public std::exception
-		{
-			public:
-				virtual const char	*what() const throw();
-		};
-		
+	
 		Form();
 		void	handleGradeException() const;
 		void	handleHighGradeException() const;
@@ -35,6 +27,26 @@ class Form
 		Form(const Form& form);
 		~Form();
 		Form& operator=(const Form& form);
+
+		const std::string&	getName() const;
+		const bool&			getIsSigned() const;
+		const int&			getSignableGrade() const;
+		const int&			getExecutableGrade() const;		
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char	*what() const throw();
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char	*what() const throw();
+		};
+
+		void	beSigned(Bureaucrat bureaucrat);
 };
+
+std::ostream&	operator<<(std::ostream &out, const Form &form);
 
 #endif
