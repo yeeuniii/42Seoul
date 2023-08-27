d3c525bb03839c9db797873033952db0119f8658
yeeuniii <anna862700@gmail.com>
2023-08-27 16:58:26 +0900

    Test(CPP06): ex00 main문 수정

--- Circle5/CPP06/ex00/main.cpp
+++ Circle5/CPP06/ex00/main.cpp
@@ -12,12 +12,9 @@ int	main(int argc, char *argv[])
 {
 	if (argc != 2)
 		return displayManual();
+	
 	static ScalarConverter converter(argv[1]);
 
-	// std::cout << argv[1] << " : " << converter.getType() << std::endl;
-	
-	converter.convertScalar();
 	converter.display();
-
 	return 0;
 }
