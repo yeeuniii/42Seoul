1e3a6f22f99078584a510e5c64388307cc30bc8e
yeeuniii <anna862700@gmail.com>
2023-09-01 16:40:35 +0900

    Feat(CPP07): ex01 iter 함수 템플릿 매개변수로 배열의 첫번째 주소 받도록 수정

--- Circle5/CPP07/ex01/iter.hpp
+++ Circle5/CPP07/ex01/iter.hpp
@@ -2,10 +2,8 @@
 #define __ITER_HPP__
 
 template <typename arrayType, typename functionType>
-void iter(arrayType* arrayAddress, unsigned int size, functionType function)
+void iter(arrayType array, unsigned int size, functionType function)
 {
-	arrayType	array = *arrayAddress;
-
 	for (unsigned int idx = 0; idx < size; idx++)
 		function(array[idx]);
 }
