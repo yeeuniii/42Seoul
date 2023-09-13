565105391f37c03f249e9f74854de65d70a12f53
yeeuniii <anna862700@gmail.com>
2023-09-13 16:54:48 +0900

    Feat(CPP09): ex00 input file 처리 함수 구현 및 잘못된 input에 대한 exception 클래스 생성

--- Circle5/CPP09/ex00/BitcoinExchange.cpp
+++ Circle5/CPP09/ex00/BitcoinExchange.cpp
@@ -1,14 +1,51 @@
 #include "BitcoinExchange.hpp"
 #include <iostream>
 #include <sstream>
-#include <utility>
+#include <cstring>
+
+BitcoinExchange::BadInput::BadInput(const std::string& detail)
+{
+	std::string	message;
+
+	message = "Error: bad input => " + detail;
+	this->message = message.c_str();
+}
+
+const char* BitcoinExchange::BadInput::what() const throw()
+{
+	return this->message;
+}
+
+BitcoinExchange::NotValidDate::NotValidDate(const std::string& detail) : BadInput(detail)
+{
+	std::string	message;
+
+	message = "Error: non-existent date. => " + detail;
+	this->message = message.c_str();
+}
+
+BitcoinExchange::NotValidValue::NotValidValue(const std::string& detail) : BadInput(detail)
+{
+	std::string	message;
+
+	message = "Error: " + detail;
+	this->message = message.c_str();
+}
 
 BitcoinExchange::BitcoinExchange() {}
 		
 BitcoinExchange::BitcoinExchange(const std::string& fileName)
 {
-	readInputFile(fileName);
-	readDataBase();
+	try
+	{
+		handleInputFile(fileName);
+		readDataBase();
+	}
+	catch(std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		std::cout << "error" << std::endl;
+	}
 }
 
 BitcoinExchange::BitcoinExchange(const BitcoinExchange& bitcoinExchange)
@@ -16,7 +53,7 @@ BitcoinExchange::BitcoinExchange(const BitcoinExchange& bitcoinExchange)
 	*this = bitcoinExchange;
 }
 
-BitcoinExchange::~BitcoinExchange() {}
+BitcoinExchange::~BitcoinExchange() {std::cout << "destruct" << std::endl;}
 
 BitcoinExchange&	BitcoinExchange::operator=(const BitcoinExchange& bitcoinExchange)
 {
@@ -42,14 +79,15 @@ void	BitcoinExchange::readDataBase()
 	{
 		std::getline(database, date, ',');
 		std::getline(database, rate, '\n');
-		this->data.insert(std::make_pair(date, convertfloat(rate)));
+		this->data.insert(std::make_pair(date, convertFloat(rate)));
 	}
+	database.close();
 	// std::map<std::string, float>::iterator itr = this->data.begin();
 	// for(; itr != this->data.end(); itr++)
 	// 	std::cout << itr->first << "," << itr->second << std::endl;
 }
 
-float	BitcoinExchange::convertfloat(std::string str) const
+float	BitcoinExchange::convertFloat(std::string str) const
 {
 	float	_float;
 	
@@ -60,7 +98,63 @@ float	BitcoinExchange::convertfloat(std::string str) const
 	return _float;
 }
 
-void	BitcoinExchange::readInputFile(const std::string& fileName)
+void	BitcoinExchange::handleInputFile(const std::string& fileName)
 {
-	(void)fileName;
+	std::ifstream	inputFile;
+
+	openInputFile(fileName, inputFile);
+	readInputFile(inputFile);
+	inputFile.close();
+}
+
+void	BitcoinExchange::openInputFile(const std::string& fileName, std::ifstream &inputFile)
+{
+	inputFile.open(fileName);
+	if (inputFile.is_open() == false)
+		throw std::ifstream::failure("Error: could not open file.");
+}
+
+void	BitcoinExchange::readInputFile(std::ifstream &inputFile)
+{
+	std::string		line;
+
+	std::getline(inputFile, line);
+	if (line != "date | value")
+		throw (BadInput("Input file must start with 'date | value'"));
+	while (inputFile.eof() == false)
+	{
+		std::getline(inputFile, line);
+		this->input.insert(makeInputPair(line));
+	}
+	// std::multimap<std::string, float>::iterator itr = this->input.begin();
+	// for(; itr != this->input.end(); itr++)
+	// 	std::cout << itr->first << " | " << itr->second << std::endl;
+}
+
+std::pair<std::string, float>	BitcoinExchange::makeInputPair(const std::string &line)
+{
+	std::string	date;
+	std::string	value;
+
+	if (line.size() < 14)
+		throw (BadInput(line));
+	date = line.substr(0, 10);
+	value = line.substr(13);
+	if (isValidDate(date) == false)
+		throw (NotValidDate(date));
+	if (isValidValue(value) == false)
+		throw (NotValidValue(value));
+	return std::make_pair(date, convertFloat(value));
+}
+
+bool	BitcoinExchange::isValidDate(const std::string& date)
+{
+	(void)date;
+	return true;
+}
+
+bool	BitcoinExchange::isValidValue(const std::string& value)
+{
+	(void)value;
+	return true;
 }
--- Circle5/CPP09/ex00/BitcoinExchange.hpp
+++ Circle5/CPP09/ex00/BitcoinExchange.hpp
@@ -4,6 +4,8 @@
 #include <map>
 #include <string>
 #include <fstream>
+#include <exception>
+#include <utility>
 
 #define DB "data.csv"
 
@@ -12,18 +14,43 @@ class BitcoinExchange
 	private:
 		std::map<std::string, float>		data;
 		std::multimap<std::string, float>	input;
+
+		class BadInput : public std::exception
+		{
+			protected:
+				const char*	message;
+			public:
+				BadInput(const std::string&);
+				const char*	what() const throw();
+		};
+		class NotValidDate : public BadInput
+		{
+			public:
+				NotValidDate(const std::string&);
+		};
+		class NotValidValue : public BadInput
+		{
+			public:
+				NotValidValue(const std::string&);
+		};
+
 		BitcoinExchange();
 		
-		float	convertfloat(std::string str) const;
-
-		void	readInputFile(const std::string& fileName);
+		void	handleInputFile(const std::string& fileName);
+		void	openInputFile(const std::string&, std::ifstream&);
+		void	readInputFile(std::ifstream&);
+		std::pair<std::string, float>	makeInputPair(const std::string&);
+		bool	isValidDate(const std::string&);
+		bool	isValidValue(const std::string&);
+		
 		void	readDataBase();
+		float	convertFloat(std::string) const;
 
 	public:
-		BitcoinExchange(const std::string& fileName);
-		BitcoinExchange(const BitcoinExchange& bitcoinExchange);
+		BitcoinExchange(const std::string&);
+		BitcoinExchange(const BitcoinExchange&);
 		~BitcoinExchange();
-		BitcoinExchange& operator=(const BitcoinExchange& bitcoinExchange);
+		BitcoinExchange& operator=(const BitcoinExchange&);
 
 		void	exchange();
 };
