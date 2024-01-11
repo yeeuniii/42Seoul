20afd641f87d04c66ad0e818b1e1e6c34fbe00f2
yeeuniii <anna862700@gmail.com>
2024-01-11 16:47:33 +0900

    feat(Inception): mariaDB root 비밀번호 설정

--- Circle5/Inception/srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ Circle5/Inception/srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,7 +1,11 @@
+#export MARIADB_ID=yeepark
+#export MARIADB_PASSWORD=password
 service mariadb start
 mysql -e "CREATE DATABASE wordpress_db;"
 mysql -e "CREATE USER '${MARIADB_ID}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
 mysql -e "GRANT ALL ON wordpress_db.* TO '${MARIADB_ID}'@'%';";
+mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MARIADB_PASSWORD}');"
 mysql -e "FLUSH PRIVILEGES;"
-service mariadb stop
+mysqladmin -uroot -ppassword shutdown
+#service mariadb stop
 mysqld
