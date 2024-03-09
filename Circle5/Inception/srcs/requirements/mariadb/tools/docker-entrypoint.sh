bfac2fc413d308859db8464bbb7e4b9de1679329
yeeuniii <anna862700@gmail.com>
2024-03-09 18:48:33 +0900

    refactor(Inception): mariadb 주석 및 불필요한 부분 제거

--- Circle5/Inception/srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ Circle5/Inception/srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
 service mariadb start
-
 chmod +x /scripts/init-user.sql
-
-# 환경 변수 값을 포함한 새 SQL 파일 생성
 envsubst < /scripts/init-user.sql > /tmp/init-user-filled.sql
-
 cat /tmp/init-user-filled.sql | mysql -uroot -p"$MARIADB_PASSWORD"
-# mysql -uroot -p"$MARIADB_PASSWORD" < /tmp/init-db-filled.sql
-
 sleep 1
 mysqladmin -uroot -p"$MARIADB_PASSWORD" shutdown
 mysqld
