c703b03af10599d82ae1a8b10f82d930fc0e2775
yeeuniii <anna862700@gmail.com>
2024-03-18 13:45:44 +0900

    feat(Inception): wordpress 설치 완료 및 관리자, 사용자 설정

--- Circle5/Inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ Circle5/Inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,6 +1,31 @@
-#!bin/sh
-sudo -u wordpressmanager -i -- wp config create --path=${WORDPRESS_PATH} \
-										--dbname=${MARIADB_DATABASE} \
-										--dbuser=${MARIADB_USER} \
-										--dbpass=${MARIADB_USER_PASSWORD} \
-										--dbhost=mariadb
+#!/bin/sh
+useradd -m wordpressmanager
+chown -R wordpressmanager:wordpressmanager ${WORDPRESS_PATH}
+
+if [ ! -f ${WORDPRESS_PATH}/index.php ]; then
+	sudo -u wordpressmanager -i -- wp core download \
+								--path=${WORDPRESS_PATH} \
+								--locale=ko_KR # en_US
+	sudo -u wordpressmanager -i -- wp config create \
+								--path=${WORDPRESS_PATH} \
+								--dbname=${MARIADB_DATABASE} \
+								--dbuser=${MARIADB_USER} \
+								--dbpass=${MARIADB_USER_PASSWORD} \
+								--dbhost=mariadb
+	sudo -u wordpressmanager -i -- wp core install \
+								--path=${WORDPRESS_PATH} \
+								--url=${WORDPRESS_URI} \
+								--title=${WORDPRESS_TITLE} \
+								--admin_user=${WORDPRESS_ADMIN_USER} \
+								--admin_password=${WORDPRESS_ADMIN_PASSWORD} \
+								--admin_email=${WORDPRESS_ADMIN_EMAIL} --skip-email
+	sudo -u wordpressmanager -i -- wp user create \
+								${WORDPRESS_USER_LOGIN} ${WORDPRESS_USER_EMAIL} \
+								--path=${WORDPRESS_PATH} \
+								--user_pass=${WORDPRESS_USER_PASSWORD} \
+								--role=author
+fi
+
+chown -R www-data:www-data ${WORDPRESS_PATH}
+
+exec php-fpm7.4 -F
--- Circle5/Inception/srcs/requirements/wordpress/tools/run_wpcli.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-
-useradd -m wordpressmanager
-mkdir /wordpress
-chown -R wordpressmanager:wordpressmanager /wordpress
-sudo -u wordpressmanager -i -- wp core download --locale=ko_KR --path=${WORDPRESS_PATH}
