359c7cf73863c9c447462d8da40e2819097d66f7
yeeuniii <anna862700@gmail.com>
2024-03-18 16:57:35 +0900

    refactor(Inception): maraidb 실행 mysqld_safe로 변경

--- Circle5/Inception/srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ Circle5/Inception/srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mysqld_safe -uroot&
+mysqld_safe &
 until mysqladmin ping -hlocalhost -uroot > /dev/null 2>&1; do
     echo "Waiting for mysql to run"
     sleep 1
@@ -7,4 +7,4 @@ done
 envsubst < /scripts/init-user.sql > /scripts/init-user-filled.sql
 mysql -uroot -p${MARIADB_ROOT_PASSWORD} < /scripts/init-user-filled.sql
 mysqladmin -uroot -p${MARIADB_ROOT_PASSWORD} shutdown
-exec mysqld --user=mysql
+exec mysqld_safe --user=mysql
